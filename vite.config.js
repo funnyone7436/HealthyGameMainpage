afb45e6ae3b979b9adbe7a6cc70ef965481dbf3d
Tiger Me <93956183+funnyone7436@users.noreply.github.com>
2026-02-03 10:41:36 -0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// ❗ replace YOUR_GH_USERNAME and YOUR_REPO_NAME
 export default defineConfig({
-  plugins: [react()],
-  base: '/HealthyGameMainpage/', // e.g. '/kids-healthy-games/'
+  base: '/HealthyGameMainpage/', 
+  plugins: [react()]
 })
 
